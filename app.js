ea850c7e72f8a40bdbffe8c6ef10c2dbf9c7bca7
Mark Wolfe <mark@wolfe.id.au>
2013-02-18 21:36:09 +1100

    Added a bit of testing and moving stuff around to enable more.

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 var CollectD = require('./lib/collectd');
 
-var collectd = new CollectD();
+var err = null
+    , metrics = null
+    , collectd = new CollectD();
 
-var metrics = [
-    'trogdor.campjs.com/interface-en1/if_errors'
-    , 'trogdor.campjs.com/interface-en1/if_octets'
-    , 'trogdor.campjs.com/interface-en1/if_packets'
-    , 'trogdor.campjs.com/load/load'
-    , 'trogdor.campjs.com/memory/memory-active'
-    , 'trogdor.campjs.com/memory/memory-free'
-    , 'trogdor.campjs.com/memory/memory-inactive'
-    , 'trogdor.campjs.com/memory/memory-wired'
-];
+collectd.getMetricList(function(err, metrics){
 
-collectd.start(metrics, 10000);
+    var startErr = null;
+
+    if (err){
+        throw new Error(err);
+    } else {
+        collectd.start(startErr, metrics, 10000);
+
+        if(startErr){
+            throw new Error(startErr);
+        }
+
+    }
+
+})
 
 
--- test/collectd_test.js
+++ test/collectd_test.js
@@ -1,2 +1,12 @@
-var collectd = require('../lib/collectd.js');
+var Chai = require('chai')
+    , metricStorage = require('../lib/collectd')
+    , util = require('util');
+
+// Test shortcuts
+
+var expect = Chai.expect;
+
+// Declare internals
+
+var internals = {};
 
