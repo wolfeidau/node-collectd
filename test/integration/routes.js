8dc927b119e29f3e0c33b83f7247c3288e106182
Mark Wolfe <mark@wolfe.id.au>
2013-03-11 10:48:09 +1100

    Messing around with testing and structure.

---
+++ test/integration/routes.js
@@ -0,0 +1,15 @@
+// Load modules
+
+var Chai = require('chai');
+var Shot = require('shot');
+var CollectD = require('../..');
+
+// Test shortcuts
+
+var expect = Chai.expect;
+
+describe('Routes', function () {
+
+
+
+});
